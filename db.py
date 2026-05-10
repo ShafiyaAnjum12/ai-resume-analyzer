214e04a83efc4f92acbcc4ac0b75c95ee5266525
ShafiyaAnjum12 <shafiyasiddique12@gmail.com>
2026-05-10 20:18:53 +0530

    Add files via upload

--- db.py
+++ db.py
@@ -7,9 +7,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True
+    pool_pre_ping=True,
+    connect_args={
+        "ssl": {}
+    }
 )
-
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
