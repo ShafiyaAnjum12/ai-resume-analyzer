a623fc1e290255bf2edce7c99c96c5c49c9304a0
ShafiyaAnjum12 <shafiyasiddique12@gmail.com>
2026-05-21 18:44:21 +0530

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class User(Base):
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(255), unique=True, nullable=False)
+    username = Column(String(255), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
 
 class Report(Base):
